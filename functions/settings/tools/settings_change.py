7ad1a27288b98f3b843572dba70015a312295850
Daniel <hora.daniel@protonmail.com>
2025-08-14 01:02:31 +0200

    Delete functions directory

--- functions/settings/tools/settings_change.py
+++
@@ -1,9 +0,0 @@
-import json
-
-# --- Function that changes app settings ---
-def settings_change(type,arg):
-    with open("app_data.json","r") as file:
-        app_data=json.load(file)
-    app_data[type]=arg
-    with open("app_data.json","w") as file:
-        json.dump(app_data, file,indent=4)
